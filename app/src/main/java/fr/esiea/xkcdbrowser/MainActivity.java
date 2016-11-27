487417140e2ef1bfe09e1076767829afd7dc82de
Gregory Le <le@et.esiea.fr>
2016-11-27 12:05:49 +0100

    Adapted navigation menu

--- app/src/main/java/fr/esiea/xkcdbrowser/MainActivity.java
+++ app/src/main/java/fr/esiea/xkcdbrowser/MainActivity.java
@@ -106,17 +106,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_browse) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_fav) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_info) {
 
         }
 
