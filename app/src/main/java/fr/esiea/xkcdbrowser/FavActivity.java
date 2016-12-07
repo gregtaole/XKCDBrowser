c68e9776b42429de37e716fec0ec87bc6c432e23
avignat <armand@vignat.org>
2016-12-07 23:54:01 +0100

    FavActivity added

--- app/src/main/java/fr/esiea/xkcdbrowser/FavActivity.java
+++ app/src/main/java/fr/esiea/xkcdbrowser/FavActivity.java
@@ -1,42 +1,33 @@
 package fr.esiea.xkcdbrowser;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.Settings;
-import android.support.v4.app.DialogFragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, NetworkConnectivityDialogFragment.NetworkConnectivityListener {
-    private ArrayList<Comic> comics = new ArrayList<>();
+public class FavActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, NetworkConnectivityDialogFragment.NetworkConnectivityListener {
+    private ArrayList<Comic> favComics = new ArrayList<>();
     private RecyclerView comicRecycler;
     private ComicAdapter comicAdapter;
     private DividerItemDecoration comicRecyclerDivider;
-    private ConnectivityManager connectivityManager;
-    private NetworkInfo networkInfo;
-    private int lastId;
-
-    public static final String EXTRA_COMIC = "fr.esiea.xkcdbrowser.COMIC";
 
     final static String TAG = "MainActivity";
 
@@ -56,11 +47,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        networkInfo = connectivityManager.getActiveNetworkInfo();
-
         comicRecycler = (RecyclerView) findViewById(R.id.main_recycler_view);
-        comicAdapter = new ComicAdapter(comics);
+        comicAdapter = new ComicAdapter(favComics);
         LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(getApplicationContext());
         comicRecyclerDivider = new DividerItemDecoration(comicRecycler.getContext(), recyclerLayoutManager.getOrientation());
         comicRecycler.setLayoutManager(recyclerLayoutManager);
@@ -80,36 +68,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             @Override
             public void onLoadMore() {
-                loadMoreComics();
+
             }
         });
         comicAdapter.setClickListener(new RecyclerViewItemClickListener() {
             @Override
             public void itemClicked(View view, int position) {
-                Intent intent = new Intent(view.getContext(), ComicViewerActivity.class);
-                intent.putExtra(EXTRA_COMIC, comics.get(position));
                 Log.d(TAG, "Click !");
-                startActivity(intent);
             }
         });
 
-        if (networkInfo != null && networkInfo.isConnected()) {
-            loadComics();
-        } else {
-            DialogFragment dialogFragment = new NetworkConnectivityDialogFragment();
-            dialogFragment.show(this.getSupportFragmentManager(), "NetworkConnectivityDialog");
-        }
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        networkInfo = connectivityManager.getActiveNetworkInfo();
-        if (!(networkInfo != null && networkInfo.isConnected())) {
-            DialogFragment dialogFragment = new NetworkConnectivityDialogFragment();
-            dialogFragment.show(this.getSupportFragmentManager(), "NetworkConnectivityDialog");
-        }
+        loadComics();
     }
 
     @Override
@@ -178,59 +147,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         startActivity(new Intent(Settings.ACTION_DATA_ROAMING_SETTINGS));
     }
 
-    private void loadMoreComics() {
-        comicAdapter.setShowLoader(true);
-        comicAdapter.notifyDataSetChanged();
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                loadComics();
-                comicAdapter.setShowLoader(false);
-                comicAdapter.notifyDataSetChanged();
-            }
-        }, 3000);
-    }
-
     private void loadComics() {
-        if (this.comics.size() == 0) {
-            try {
-                lastId = new ComicFetcher().execute("http://xkcd.com/info.0.json").get().getId();
-            } catch (InterruptedException | ExecutionException e) {
-                Log.d(TAG, e.getLocalizedMessage());
-            }
-        }
-        loadNNext(10, lastId);
-        lastId -= 10;
-    }
-
-    private void loadNNext(int n, int lastId) {
-        for (int i = lastId - 1; i > lastId - 1 - n ; i--) {
-            if (i <= 0)
-                break;
-            new ComicFetcher().execute("http://xkcd.com/" + String.valueOf(i) +"/info.0.json");
-        }
-    }
-
-    private class ComicFetcher extends AsyncTask<String, Integer, Comic> {
-        @Override
-        protected Comic doInBackground(String... urls) {
-            ComicBuilder builder = ComicBuilder.getInstance();
-            Comic newComic = builder.buildComic(urls[0]);
-            Log.d("MainActivity", newComic.getTitle());
-            if (newComic != null) {
-                return newComic;
-            }
-            else {
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Comic result) {
-            comics.add(result);
-            comicAdapter.notifyDataSetChanged();
-        }
+        //Load fav here
     }
 }
