cc3fe89bd21e69437adae30f4f48b4903efd7ea8
Gregory Le <le@et.esiea.fr>
2016-12-17 09:48:08 +0100

    Added click on notif and blinking LED

--- app/src/main/java/fr/esiea/xkcdbrowser/NewComicBR.java
+++ app/src/main/java/fr/esiea/xkcdbrowser/NewComicBR.java
@@ -1,6 +1,7 @@
 package fr.esiea.xkcdbrowser;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -17,10 +18,20 @@ public class NewComicBR extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive");
+        Intent startAppIntent = new Intent(context, MainActivity.class);
+        PendingIntent startAppPendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                startAppIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_menu_fav)
                 .setContentTitle("XKCDBrowser")
-                .setContentText("New comic(s)!");
+                .setContentText("New comic(s)!")
+                .setLights(0xff3F51B5 , 1000, 1000)
+                .setAutoCancel(true)
+                .setContentIntent(startAppPendingIntent);
 
         NotificationManager mNotifyMgr =
                 (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
